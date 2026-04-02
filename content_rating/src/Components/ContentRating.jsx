e70517393d43f0493d877388de2a4c45c1bc9aee
Ansonclh <ansoncheng1116@hotmail.com>
2026-04-02 16:32:21 +0800

    message

--- content_rating/src/Components/ContentRating.jsx
+++ content_rating/src/Components/ContentRating.jsx
@@ -5,11 +5,33 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      total_ratings: 0,
+      handleLike: () => {
+        this.setState({ likes: this.state.likes + 1, total_ratings: this.state.total_ratings + 1 });
+      },
+      handleDislike: () => {
+        this.setState({ dislikes: this.state.dislikes + 1, total_ratings: this.state.total_ratings + 1 });
+      }
+    }
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+
+      <div className='content-rating'>
+          <p>//Add text here</p>
+
+          <div className='rating-buttons'>
+              <button className="like-button" onClick={this.state.handleLike}>Like ({this.state.likes})</button>
+              <button className="dislike-button" onClick={this.state.handleDislike}>Dislike ({this.state.dislikes})</button>
+              <p>Total Ratings: {this.state.total_ratings}</p>
+
+          </div>
+      </div>
+
      </>
     );
   }
