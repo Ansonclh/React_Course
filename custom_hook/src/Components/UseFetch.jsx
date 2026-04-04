773aff929ca0f469c4f1ad6ce7c3dd89f3146fb4
Ansonclh <ansoncheng1116@hotmail.com>
2026-04-04 17:42:20 +0800

    message

--- custom_hook/src/Components/UseFetch.jsx
+++ custom_hook/src/Components/UseFetch.jsx
@@ -1,5 +1,15 @@
+import { useState, useEffect } from "react";
+
 const UseFetch = (url) => {
-    
+    const[data,setData]=useState();
+     useEffect(()=>{
+            fetch(url)
+            .then((res)=>res.json())
+            .then((data)=>setData(data))
+            .catch((e) => console.log(e))
+        },[])
+    return [data]
+
 }
 
 export default UseFetch
