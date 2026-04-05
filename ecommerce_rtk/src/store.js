4285fa2b1b80a142656ecd4d8f9ee94aad5584d5
Ansonclh <ansoncheng1116@hotmail.com>
2026-04-05 22:46:04 +0800

    message

--- ecommerce_rtk/src/store.js
+++ ecommerce_rtk/src/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './Components/CartSlice';
+import avReducer from './avSlice';
+import venueReducer from './venueSlice';
+
 const store = configureStore({  //centralized store for application
   reducer: {
     cart: cartReducer,
+    av: avReducer,
+    venue: venueReducer,
   },
 });
 
